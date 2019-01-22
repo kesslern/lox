eb57a912f57f05321dce206f30a939937ba70341
Nathan Kessler <nathant93@gmail.com>
2019-01-21 20:01:26 -0500

    Use constructor variable initialization

--- src/Token.ts
+++ src/Token.ts
@@ -1,17 +1,12 @@
 import TokenType from './TokenType'
 
 export default class Token {
-  public type: TokenType
-  public lexeme: string
-  public literal: string | number
-  public line: number
-
-  constructor(type: TokenType, lexeme: string, literal: string | number, line: number) {
-    this.type = type
-    this.lexeme = lexeme
-    this.literal = literal
-    this.line = line
-  }
+  constructor(
+    readonly type: TokenType,
+    readonly lexeme: string,
+    readonly literal: string | number,
+    readonly line: number,
+  ) {}
 
   public toString = (): string => `${this.type} ${this.lexeme} ${this.literal}`
 }
